0890a11828f69d56b56a30262102d12dd82aa86b
Emma <emmaolssondev@outlook.com>
2022-05-05 08:53:28 +0200

    lagt till funktion för att lägga till member

--- server.js
+++ server.js
@@ -1,37 +1,49 @@
 import express from 'express';
-import { MongoClient } from "mongodb";
+import { MongoClient,ObjectId } from "mongodb";
 const port = 3000;
 const app = express();
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.static('./public'))
+app.use(express.urlencoded());
 
 const client = new MongoClient('mongodb://localhost:27017');
 await client.connect();
 const db = client.db('library');
-const membercollection = db.collection('members');
+const memberCollection = db.collection('members');
 
+//läser alla sidor
 app.get('/startpage', async (req, res) => {
-    const startpage = await membercollection.find({}).toArray();
+    const startpage = await memberCollection.find({}).toArray();
     res.render('startpage', { startpage });
 });
 app.get('/member', async (req, res) => {
-    const member = await membercollection.find({}).toArray();
+    const member = await memberCollection.find({}).toArray();
     res.render('member', { member });
 });
 app.get('/newmember', async (req, res) => {
-    const newmember = await membercollection.find({}).toArray();
+    const newmember = await memberCollection.find({}).toArray();
     res.render('newmember', { newmember });
 });
 app.get('/allmembers', async (req, res) => {
-    const allmembers = await membercollection.find({}).toArray();
+    const allmembers = await memberCollection.find({}).toArray();
     res.render('allmembers', { allmembers });
 });
 
-// app.get('/member/:id', async (req, res) => {
-//     const memberid = await membercollection.findOne({ _id: ObjectId(req.params.id) });
-//     res.render('memberid', memberid);
-//   });
+//läser en användare utifrån id
+app.get('/member/:id', async (req, res) => {
+    const memberID = await memberCollection.findOne({ _id: ObjectId(req.params.id) });
+    res.render('member',memberID);
+  });
+
+// post en ny användare
+app.get('/newmember', (req, res) => {
+    res.render('newmember');
+  });
+  
+  app.post('/newmember', async (req, res) => {await memberCollection.insertOne(req.body);
+    res.redirect('/allmembers');
+  });
 
 app.listen(port, () => console.log(`Listening on ${port}`));
